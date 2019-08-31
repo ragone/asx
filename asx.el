82f61c6238c688d239f160223735f98de8f9f6fe
Alex Ragone <ragonedk@gmail.com>
2019-08-31 20:39:22 +0200

    docs: updated documentation

--- asx.el
+++ asx.el
@@ -26,7 +26,8 @@
 
 ;; This package allows you to search any StackExchange site specified in
 ;; `asx-sites' and insert the top post in an Org-mode buffer. For better searching,
-;; this uses Google as it is superior to StackExchange's searching capabilities.
+;; this uses Google and DuckDuckGo as it is superior to StackExchange's searching
+;; capabilities. More search engines can be defined using `asx-search-engine-alist'.
 ;;
 ;; # Usage
 ;; Run ~M-x asx~ and enter query when prompted.
